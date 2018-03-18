313e2823f8c21b67f575efb928201456167b5e16
MaximeRebillard <rebillardmaxime@gmail.com>
2018-03-18 16:27:11 +0100

    stop message handling for i2c.js

--- i2c.js
+++ i2c.js
@@ -1,35 +1,29 @@
 
 /*
 
-install with npm : npm install raspi-i2c
+install with npm :
+npm install .
+npm install raspi-i2c
+npm install raspi
 
-il faudrait faire 2 fichiers :
-	- start.js
-	- stop.js
+3 modes :
+	- start (1)
+	- sendData(2)
+	- stop (3)
 
-sur le portail lorsque on lance la partie :
-on appel : "node start.js tempo valeurs
-
-avec tempo : entier 
-valeurs : tableaux de int (0 ou 1)
-
-simulation :(fonctionelle)
-
-To Start : call "node i2c.js 1"
-To SendData : " call "node i2c.js 2 110101"
+commandline :
+	- to Start : "node i2c.js 1"
+	- to Send specific data : " call "node i2c.js 2 110101"
+	- to Stop : "node i2c.js 3"
 
 */
-
-//modules
 
 const raspi = require('raspi');
 const I2C = require('raspi-i2c').I2C;
 
-//example de données (1ere : temp, les 6 autres les données)
 var data ;
 var values;
 var tempo;
-
 
 
 function start(i2c){
@@ -50,49 +44,64 @@ i2c.writeSync(0x12,starting_buffer);
 }
 
 function sendData(i2c){
-
 //values for the leds
 values = new Uint8Array(6);
 for(var i = 0; i < values.length; i++){
 
 values[i] = data[i];
 console.log(values[i]);
+
 }
 const buf = new Buffer(values.buffer);
-
 i2c.writeSync(0x12,buf);
 //i2c.writeByteSync(0x12,values);
 
 }
 
+
 function stop(i2c){
 
-const stopping_buffer = new Buffer.from('stop','utf8');
+const stopping_buffer = new Buffer('stop','utf8');
 i2c.writeSync(0x12,stopping_buffer);
+
 }
 
-//var buff = new Buffer([0,0,1,1,0,1]);
-
-//start =>  0
-//stop =>  1
 
 raspi.init(() => {
 const i2c = new I2C();
-//  console.log(i2c.readByteSync(0x12)); // Read one byte from the device at address 12 i2c.
-//wordToByteArray();
 
-if(process.argv[2] == 1) start(i2c);
-if(process.argv[2] == 2) {
-data = (process.argv[3]).split('');
+if(process.argv[2] == 1){
+start(i2c);
+}
+else if(process.argv[2] == 2){
+data = process.argv[3].split('');
 console.log(data.toString());
 sendData(i2c);
 }
-if(process.argv[2] == 3) stop(i2c);
+else if(process.argv[2] == 3){
+stop(i2c);
+}
+else console.log("please check the parameters");
+/*
 
+//WITH A SWITCH IT DOESN'T WORK ...
 
-//i2c.writeByteSync(0x12,64);
-//i2c.writeWordSync(0x12,tab);
+switch(process.argv[2]){
+case '1' :
+start(i2c);
+break;
+case '2':
+data = (process.argv[3]).split('');
+console.log(data.toString());
+sendData(i2c);
+case '3' :
+stop(i2c);
+break;
+default:
+console.log("please check the parameters");
+}
 
+*/
 
 });
 
